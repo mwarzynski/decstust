c4c90abaff5d0eb36f81c1a9cc4c554b95f618aa
Mateusz Warzyński <mateusz@warzynski.pro>
2022-10-23 18:47:31 +0200

    feat: initial 'design' in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+// Goal: Component which ensures declarative state is reflected in the real-world environment.
+//
+// Components:
+//  - Objects manager in CQRS (command query responsibility segregation)
+//      - Query to understand current state,
+//      - Command to apply actions in order to achieve desired state,
+//  - Desired State Declaration Store
+//      - Store which allows to keep the state declaration specified by the user,
+//
+//  - Actions Planner
+//      - compares current real-world state with the declaration and outputs actions needed to
+//      achieve desired state
+
 fn main() {
     println!("Hello, world!");
 }
